879812f9cf1e87dbf7b9b7b2b337e83091cdab95
Pietro Pasotti <starfire.daemon@gmail.com>
2022-06-27 15:27:46 +0200

    fix to update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,11 @@
-tox ./libs/"$1" || echo "TESTS FAILED! aborting..." && exit 1
-PYTHONPATH=$PYTHONPATH:./ ./scripts/bump-version.py "$1"
-PYTHONPATH=$PYTHONPATH:./ ./scripts/inline-lib.py "$1"
-./scripts/publish.sh $1
+tox ./libs/"$1"
+
+if [ $? -eq 0 ]; then
+  PYTHONPATH=$PYTHONPATH:./ ./scripts/bump-version.py "$1"
+  PYTHONPATH=$PYTHONPATH:./ ./scripts/inline-lib.py "$1"
+  ./scripts/publish.sh $1
+  echo "FOO"
+else
+    echo "TESTS FAILED! aborting..."
+fi
+
