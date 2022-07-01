ecd9e12710bc429fdea458a7e8f6f295c88e3615
Pietro Pasotti <starfire.daemon@gmail.com>
2022-07-01 11:40:27 +0200

    added relation context

--- libs/harness_ctx/harness_ctx.py
+++ libs/harness_ctx/harness_ctx.py
@@ -1,8 +1,12 @@
-import typing
-from typing import Callable, Protocol, Type
+from contextlib import contextmanager, ExitStack
+from dataclasses import dataclass
+from typing import Callable, Protocol, Type, Tuple, cast, TypedDict, Dict, \
+    Literal, TypeVar, List, Any, Generator, Optional, Union, ContextManager
 
+import yaml
 from ops.charm import CharmBase, CharmEvents
 from ops.framework import BoundEvent, Handle
+from ops.model import Relation
 from ops.testing import Harness
 
 
@@ -62,12 +66,12 @@ class HarnessCtx:
     """
 
     def __init__(
-        self,
-        charm: Type[CharmBase],
-        event_name: str,
-        emitter: Callable[[CharmBase, Harness], _HasOn] = _DefaultEmitter,
-        *args,
-        **kwargs
+            self,
+            charm: Type[CharmBase],
+            event_name: str,
+            emitter: Callable[[CharmBase, Harness], _HasOn] = _DefaultEmitter,
+            *args,
+            **kwargs
     ):
         self.charm_cls = charm
         self.emitter = emitter
@@ -94,7 +98,7 @@ class HarnessCtx:
             )
             event.framework = framework
             framework._emit(event)  # type: ignore
-            return typing.cast(BoundEvent, event)
+            return cast(BoundEvent, event)
 
         self._emitter = bound_ctx = Emitter(harness, _emit)
         return bound_ctx
@@ -103,3 +107,192 @@ class HarnessCtx:
         if not self._emitter.emitted:
             self._emitter.emit()
         self._harness.framework.on.commit.emit()  # type: ignore
+
+
+DataBag = Dict[str, str]
+
+
+class Remote(TypedDict):
+    name: str
+    units: Dict[int, DataBag]
+    app_data: DataBag
+
+
+_T = TypeVar("_T", bound=Type[CharmBase])
+
+
+class EndpointCtx:
+    def __init__(self,
+                 charm_cls: _T,
+                 relation_name: str,
+                 interface: str,
+                 role: Literal['provides', 'requires'] = 'requires',
+                 # remotes: Tuple[Remote, ...] = ()
+                 ):
+        self._role = role
+        self._charm_cls = charm_cls
+        self._relation_name = relation_name
+        self._interface = interface
+        self._relation_ctxs: Dict[str, RelationCtx] = {}
+        self._begun = False
+        self._harness = None
+        # self._remotes: List[Remote] = []
+        #
+        # if remotes:
+        #     self.setup()
+        #     for remote in remotes:
+        #         self.add_remote(remote)
+
+    @property
+    def harness(self) -> Harness[_T]:
+        if not self._begun:
+            raise RuntimeError('enter the context first')
+        return self._harness
+
+    def setup(self):
+        if self._begun:
+            return
+        self._begun = True
+
+        meta = {
+            self._role: {
+                self._relation_name: {
+                    'interface': self._interface
+                }
+            }
+        }
+        harness = Harness(self._charm_cls,  # type: ignore
+                          meta=yaml.safe_dump(meta))
+        harness.begin()
+        self._harness = harness
+
+    def __enter__(self):
+        self.setup()
+        # self._exit_stack = exstck = ExitStack()
+        # exstck.__enter__()
+        return self
+
+    def __exit__(self, *exc):
+        self._harness.cleanup()
+        # for remote in self._remotes:
+        #     self.remove_remote(remote)
+        # self._exit_stack.__exit__(*exc)
+
+    # def add_remote(self, remote: Remote):
+    #     """Adds all the remotes and populates the databags."""
+    #     child_ctx_mgr = next(self.relation(remote))
+    #     self._exit_stack.enter_context(child_ctx_mgr)
+    #     self._relation_ctxs[remote['name']] = child_ctx_mgr
+    #     self._remotes.append(remote)
+    #
+    # def remove_remote(self, remote: Remote):
+    #     """Clear the databags, remove the remote."""
+    #     child_ctx = self._relation_ctxs.pop(remote['name'])
+    #     child_ctx.__exit__()
+    #     self._remotes.remove(remote)
+
+    @contextmanager
+    def relation(self, remote: Union[str, Remote]) -> ContextManager[
+        'RelationCtx']:
+        """remote: a Remote or the name of an existing remote."""
+        # if isinstance(remote, str):
+        #     try:
+        #         remote = next(r for r in self._remotes if r['name'] == remote)
+        #     except IndexError:
+        #         raise ValueError(f'remote {remote!r} not found')
+        #
+        # existing_ctx = self._relations.get(remote['name'])
+        # if existing_ctx:
+        #     yield existing_ctx
+        #     return
+        #
+        # if remote not in self._remotes:
+        #     self.add_remote(remote)
+        ctx = RelationCtx(self, remote)
+        self._relation_ctxs[remote['name']] = ctx
+        with ctx:
+            yield ctx
+        del self._relation_ctxs[remote['name']]
+        # self.remove_remote(remote)
+
+    def get_relation(self, remote_name: str):
+        return self._relation_ctxs[remote_name].relation
+
+    def update_remote(self, remote_name: str,
+                      app_data: DataBag = None,
+                      units: Dict[int, DataBag] = None):
+        self._relation_ctxs[remote_name].update(app_data, units)
+
+
+class RelationCtx:
+    def __init__(self, endpoint: EndpointCtx, remote: Remote):
+        self._endpoint = endpoint
+        self.remote = remote
+        self.relation: Optional[Relation] = None
+        self.units = []
+
+    def __enter__(self):
+        ep = self._endpoint
+        remote = self.remote
+        harness = ep.harness
+        relation_name = ep._relation_name
+
+        r_id = harness.add_relation(relation_name, remote['name'])
+        self.relation = harness.model.get_relation(relation_name, r_id)
+        self._sync_remote()
+        return self
+
+    def update(self, app_data: DataBag = None,
+               units: Dict[int, DataBag] = None):
+        """Updates the remote with this data and syncs with harness."""
+        if not self.relation:
+            raise RuntimeError('enter the context first')
+
+        # sync remote
+        remote = self.remote
+        if app_data is not None:
+            remote['app_data'] = app_data
+        if units is not None:
+            remote['units'] = units
+        self._sync_remote()
+
+    def _sync_remote(self):
+        remote_name = self.remote['name']
+        app_data = self.remote['app_data']
+        units = self.remote['units']
+
+        harness = self._endpoint.harness
+
+        # app data
+        harness.update_relation_data(self.relation.id, remote_name, app_data)
+        # remote units and their data
+        for unit in units.keys():
+            # create unit if it doesn't exist yet:
+            unit_name = f"{self.remote['name']}/{unit}"
+            if unit_name not in harness._backend._relation_app_and_units[self.relation.id]['units']:
+                self.add_unit(unit, units[unit])
+
+        for unit in self.units:
+            if unit not in units:
+                self.remove_unit(unit)
+
+    def add_unit(self, unit: int, data: DataBag):
+        remote = self.remote
+        r_id = self.relation.id
+        harness = self._endpoint.harness
+        self.units.append(unit)
+
+        remote['units'][unit] = data
+
+        unit_name = f"{remote['name']}/{unit}"
+        harness.add_relation_unit(r_id, unit_name)
+        harness.update_relation_data(r_id, unit_name, data)
+
+    def remove_unit(self, unit: int):
+        unit_name = f"{self.remote['name']}/{unit}"
+        self._endpoint.harness.remove_relation_unit(self.relation.id, unit_name)
+
+    def __exit__(self, *exc):
+        self._endpoint.harness.remove_relation(self.relation.id)
+        self.relation = None
+        self.units = []
--- libs/harness_ctx/tests/test_unit.py
+++ libs/harness_ctx/tests/test_unit.py
@@ -1,5 +1,6 @@
 import sys
 import typing
+from contextlib import ExitStack
 from pathlib import Path
 
 import pytest as pytest
@@ -9,7 +10,7 @@ from ops.framework import Framework
 lib_root = Path(__file__).parent.parent
 sys.path.append(str(lib_root))
 
-from harness_ctx import HarnessCtx
+from harness_ctx import HarnessCtx, EndpointCtx
 
 
 @pytest.fixture
@@ -31,3 +32,88 @@ def test_emit(charm_cls):
         event = h.emit()
         assert event.handle.kind == "update_status"
     assert h.harness.charm.event.handle.kind == "commit"
+
+
+def test_relation_ctx_remotes():
+    class MyCharm(CharmBase):
+        def __init__(self, framework: Framework, key: typing.Optional = None):
+            super().__init__(framework, key)
+
+        def happy(self):
+            db_relations = self.model.relations.get('database')
+            if not db_relations:
+                return False
+
+            happy = {}
+            for relation in db_relations:
+                if not relation.data[relation.app].get('a') == 'b':
+                    happy[relation.id] = False
+                    continue
+                for unit in relation.units:
+                    if unit._is_our_unit:
+                        continue
+                    udata = relation.data[unit]
+                    if not udata.get('foo') == 'bar':
+                        happy[relation.id] = False
+                        continue
+                happy[relation.id] = happy.get(relation, 'True')
+            return happy
+
+    with EndpointCtx(MyCharm, 'database', 'db', 'requires') as endpoint:
+        assert not endpoint.harness.charm.happy()
+
+        # let's add one more relation
+        with endpoint.relation({
+                        'name': 'remote_0',
+                        'units': {0: {'foo': 'bar'}},
+                        'app_data': {'a': 'b'}
+                    }):
+            # so far so good
+            assert all(endpoint.harness.charm.happy().values())
+
+            # let's add two more relations
+            with ExitStack() as stack:
+                for mgr in (endpoint.relation(remote) for remote in (
+                    {
+                        'name': 'remote_1',
+                        'units': {0: {'foo': 'bar'}},
+                        'app_data': {'a': 'c'}  # bad data!
+                    },
+                    {
+                        'name': 'remote_2',
+                        'units': {0: {'foo': 'bar'},
+                                  1: {'baz': 'qux'}},
+                        'app_data': {'a': 'b'}
+                    },
+                )):
+                    stack.enter_context(mgr)
+
+                # remote_1 app data is bork, the rest are good
+                happy = endpoint.harness.charm.happy()
+                assert happy[endpoint.get_relation('remote_0').id]
+                assert happy[endpoint.get_relation('remote_2').id]
+                assert not happy[endpoint.get_relation('remote_1').id]
+
+                # let's fix it
+                endpoint.update_remote('remote_1', app_data={'a': 'b'})
+
+                # aka:
+                if bool(eval('0')):
+                    remote_1_relation = endpoint.get_relation('remote_1')
+                    remote_1_relation.update(app_data={'a': 'b'})
+
+                    # OR:
+                    relation = endpoint.get_relation('remote_1').relation
+                    relation.data[relation.app]['a'] = 'b'
+
+                    # OR:
+                    endpoint.harness.update_relation_data(
+                        relation.id, 'remote_1', {'a': 'b'})
+
+                assert all(endpoint.harness.charm.happy().values())
+
+            # with two remotes gone, we're left with one relation
+            assert len(endpoint.harness.charm.model.relations['database']) == 1
+
+        # and now we're alone again
+        assert len(endpoint.harness.charm.model.relations['database']) == 0
